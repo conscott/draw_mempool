a6bc3b47544713bbfb91492ee03b235bdf41027a
Conor Scott <conor.r.scott.88@gmail.com>
2018-02-19 17:47:53 -0500

    Add getopt options for mempool filters

--- draw_mempool.py
+++ draw_mempool.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
+import getopt
 import time
 import copy
 import math
 import os
+import sys
 import networkx as nx
 from rpc import NodeCLI
 from matplotlib import pyplot as plt
@@ -62,9 +64,13 @@ def build_graph(base_tx, G, seen):
 # In Sat/Byte
 def get_tx_fee(tx):
     return float(mempoolinfo[tx]['fee'])*100000000.0/mempoolinfo[tx]['size']
+    # return float(mempoolinfo[tx]['ancestorfees'])*100000000.0/mempoolinfo[tx]['size']
+
+def get_tx_age_minutes(tx):
+    return (time.time()-mempoolinfo[tx]['time'])/60.0
 
 def fee_to_node_size(fee):
-    return min(1+math.log(fee, 2)*10, 1000)
+    return min(1+math.log(fee, 2)*5, 1000)
 
 def get_tx_graph(tx, draw=False):
     G = nx.DiGraph()
@@ -98,7 +104,7 @@ def draw_time_fee_tx(G, title=None):
     max_fee = max(tx_fees.values())
     min_fee = min(tx_fees.values())
 
-    tx_ages = {tx: (time.time()-mempoolinfo[tx]['time'])/60.0 for tx in G}
+    tx_ages = {tx: get_tx_age_minutes(tx) for tx in G}
     max_age = max(tx_ages.values())
     min_age = min(tx_ages.values())
 
@@ -107,6 +113,7 @@ def draw_time_fee_tx(G, title=None):
 
     G.position = {tx: (tx_ages[tx], tx_fees[tx]) for tx in G}
     nodesize = [fee_to_node_size(tx_fees[tx]) for tx in G]
+    alpha = [min(.2*mempoolinfo[tx]['ancestorcount'], 1) for tx in G]
 
     fig, ax = plt.subplots()
     fig.set_size_inches(12, 8, forward=True)
@@ -134,34 +141,63 @@ def draw_time_fee_tx(G, title=None):
 
     # nx.draw(G, G.position, with_labels=False)
     pos = G.position
-    nx.draw_networkx_nodes(G, pos, node_size=nodesize)
-    nx.draw_networkx_edges(G, pos, alpha=0.4)
+    nx.draw_networkx_nodes(G, pos, alpha=alpha, node_size=nodesize)
+    nx.draw_networkx_edges(G, pos, alpha=0.3)
     plt.show()
 
-
 def high_ancestor(tx):
-    return True if (get_tx_fee(tx) > MIN_FEE and mempoolinfo[tx]['ancestorcount'] > 15) else False
+    return tx_filter(minfee_rate=1, minancestors=15)
 
 def high_fee(tx):
-    return True if (get_tx_fee(tx) > HIGH_FEE) else False
+    return tx_filter(minfeerate=HIGH_FEE)
 
-def get_mempool_graph(filter_func=None, node_limit=1000):
+def interesting(tx):
+    return tx_filter(minfeerate=10, minancestors=4)
+
+def tx_filter(tx, minfee=0.0, maxfee=21000000,
+                  minfeerate=0, maxfeerate=21000000,
+                  minancestors=1, maxancestors=26,
+                  mindescendants=1, maxdescendants=26,
+                  minage=0, maxage=315360000,
+                  minheight=1, maxheight=21000000,
+                  minsize=1, maxsize=8000000, **kwargs):
+
+    if kwargs:
+        print("You done fucked up!")
+        print(kwargs)
+        assert(False)
+
+    tx_info = mempoolinfo[tx]
+    return (minfee <= tx_info['fee'] <= maxfee and
+            minfeerate <= get_tx_fee(tx) <= maxfeerate and
+            minancestors <= tx_info['ancestorcount'] <= maxancestors and
+            mindescendants <= tx_info['descendantcount'] <=  maxdescendants and
+            minage <= get_tx_age_minutes(tx) <= maxage and
+            minheight <= tx_info['height'] <=  maxheight and
+            minsize <= tx_info['size'] <=  maxsize)
+
+
+def get_mempool_graph(filter_func=tx_filter, txlimit=1000, **kwargs):
     G = nx.DiGraph()
     mempoolinfo_cp = copy.deepcopy(mempoolinfo)
     added = 0
-    while added < node_limit:
+    while added < txlimit:
         try:
             tx = next(iter(mempoolinfo_cp))
         except StopIteration:
             break
-        if not filter_func or filter_func(tx):
-            added += 1
+        if not filter_func or filter_func(tx, **kwargs):
             seen = add_to_graph(G, tx)
+            added += len(seen)
             for tx in seen:
                 mempoolinfo_cp.pop(tx, None)
         else:
             mempoolinfo_cp.pop(tx, None)
-    return G
+    return G if added else None
+
+
+def usage():
+    return "TODO"
 
 
 if __name__ == "__main__":
@@ -171,9 +207,25 @@ if __name__ == "__main__":
     # Get verbose mempoolinfo, TxId -> TxInfo
     mempoolinfo = rpc.getrawmempool('true')
 
-    # G_all = get_mempool_graph(node_limit=5000)
-    # draw_time_fee_tx(G_all, title='Mempool Unfiltered')
-    # G_related = get_mempool_graph(high_ancestor)
-    # draw_time_fee_tx(G_related, title='Large Dependency Transactions')
-    G_high_fee = get_mempool_graph(high_fee)
-    draw_time_fee_tx(G_high_fee, title='High Fee Transactions')
+    getopt_long_args = '''minfee= maxfee= minfeerate= maxfeerate=
+                          minheight= maxheight= minsize= maxsize=
+                          minage= maxage= minancestors= maxancestors=
+                          mindescendants= maxdescendants= txlimits='''.split()
+
+    filter_options = {}
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], [], getopt_long_args)
+    except getopt.GetoptError as err:
+        usage()
+        sys.exit(1)
+
+    # Hacky is my name
+    for opt, arg in opts:
+        filter_options[opt.replace('-', '')] = int(arg)
+
+    #print("Using filters %s" % filter_options)
+    G = get_mempool_graph(**filter_options)
+    if not G:
+        print("Filtered out all transactions, nothing to draw")
+    else:
+        draw_time_fee_tx(G, title='Mempool Filtered')
